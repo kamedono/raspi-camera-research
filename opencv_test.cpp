058b7f30cf7aa3b2a2d7996bc59d064c5f672d74
Toshiki Higaki <kamedono@i-dhcp003.info.yuge.ac.jp>
2015-12-14 19:12:00 +0900

    adding libxbee

--- opencv_test.cpp
+++ opencv_test.cpp
@@ -1,7 +1,7 @@
 /*
-http://junkroom2cyberrobotics.blogspot.jp/2012/10/raspberry-pi-opencv.html
-参考にさせていただきました。
-*/
+   http://junkroom2cyberrobotics.blogspot.jp/2012/10/raspberry-pi-opencv.html
+   参考にさせていただきました。
+ */
 
 // OpenCV カメラ画像取得テスト。
 #include <stdio.h>
@@ -14,40 +14,40 @@ http://junkroom2cyberrobotics.blogspot.jp/2012/10/raspberry-pi-opencv.html
 
 int main(int argc, char **argv)
 {
- CvCapture *capture = 0;
- IplImage *frame = 0;
- int c;
+        CvCapture *capture = 0;
+        IplImage *frame = 0;
+        int c;
 
- //double width = 160, height = 120;
- double width = 320, height = 240;
- //double width = 640, height = 480;
- //double width = 800, height = 600;
+        double width = 320, height = 240;
 
- // カメラに対するキャプチャ構造体を作成。
- if (argc == 1 || (argc == 2 && strlen(argv[1]) == 1 && isdigit(argv[1][0])))
-  capture = cvCreateCameraCapture(argc == 2 ? argv[1][0] - '0' : 0);
+        // カメラに対するキャプチャ構造体を作成。
+        if (argc == 1 || (argc == 2 && strlen(argv[1]) == 1 && isdigit(argv[1][0])))
+                capture = cvCreateCameraCapture(argc == 2 ? argv[1][0] - '0' : 0);
 
- // キャプチャサイズの設定。
- cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, width);
- cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, height);
+        // キャプチャサイズの設定。
+        cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, width);
+        cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, height);
 
- // ウィンドウ作成。
- cvNamedWindow("Capture", CV_WINDOW_AUTOSIZE);
+        // ウィンドウ作成。
+        cvNamedWindow("Capture", CV_WINDOW_AUTOSIZE);
 
- while(1)
- {
-  // 画像キャプチャ。
-  frame = cvQueryFrame(capture);
-  cvShowImage("Capture", frame);
+        while(1)
+        {
+                // 画像キャプチャ。
+                frame = cvQueryFrame(capture);
+                cvShowImage("Capture", frame);
 
-  c = cvWaitKey(2);
-  if(c == '\x1b') break;
-  //sleep(2);
- }
+                c = cvWaitKey(2);
+                if(c == '\x1b') break;
+                //sleep(2);
 
- // 後片付け。
- cvReleaseCapture(&capture);
- cvDestroyWindow("Capture");
+                // todo
+                // zigbeeの処理を追加
+        }
 
- return 0;
+        // 後片付け。
+        cvReleaseCapture(&capture);
+        cvDestroyWindow("Capture");
+
+        return 0;
 }
